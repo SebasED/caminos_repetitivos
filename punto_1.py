fc30ab8970d8207a85b0f5e98ec651e2b58d7ec2
SebasED <johns.estrada@udea.edu.co>
2022-06-07 17:04:05 -0500

    Punto_1 finished

--- punto_1.py
+++ punto_1.py
@@ -33,8 +33,26 @@
     Y así hasta que ingresen la palabra para detener el programa.
 """
 
-# Show creatures options and saves in a variable
+
+"""
+necessary variables for the program
+    creature: str
+    directin: str
+    stop: bool
+    message: str
+"""
+creature = ""
+direction = ""
+stop = False
+message = ""
+
+
+"""
+This code shows all options for creatures and save it in the creature variable. The code will repeat until
+the user chooses a correct option
+"""
 while True:
+    # Show the different criatures options
     print("\n\
     Which creature are you seeing?  \n\
         Kraken \n\
@@ -48,25 +66,60 @@ while True:
         \n\
     choose an option: ")                              
     creature = input().capitalize()
+
+    # If the user choose a validate option, the loop will finish
     if creature == "Kraken":
-        pass
+        break
+    elif creature == "Sirenas":
+        break
+    elif creature == "Ballena":
+        break
+    elif creature == "Hipocampo":
+        break
+    elif creature == "Macaraprono":
+        break
+    elif creature == "Pulpo":
+        break
+    elif creature == "Leviatanes":
+       break
+    elif creature == "Hidras":
+        break
+    else:
+        print("Choose a correct option")
 
-# Show direction options and saves in a variable 
-print("\n\
-which direction the creature is?  \n\
-    Babor \n\
-    Estribor \n\
-    Proa \n\
-    Popa \n\
-    \n\
-choose an option: ")                              
-direction = input().capitalize()
+"""
+This code shows all options for direction and save it in the creature variable. The code will repeat until
+the user chooses a correct option
+"""
+while True:
+    # Show the different directions options
+    print("\n\
+    which direction the creature is?  \n\
+        Babor \n\
+        Estribor \n\
+        Proa \n\
+        Popa \n\
+        \n\
+    choose an option: ")                              
+    direction = input().lower()
 
-# Variable for stopping the loop
-stop = True
+    # If the user choose a validate option, the loop will finish
+    if direction == "babor":
+        break
+    elif direction == "estribor":
+        break
+    elif direction == "proa":
+        break
+    elif direction == "popa":
+        break
+    else:
+        print("Choose a correct option")
 
-while stop:
-    message = ""
+"""
+This code shows the final message and repeat it until the user type the "stop" word
+"""
+while not stop:
+    # concatenate the first part of the final message based on the choossed creature
     if creature == "Kraken":
         message = "¡Ahoy! capitán, un Kraken"
     elif creature == "Sirenas":
@@ -83,7 +136,21 @@ while stop:
         message = "¡Ahoy! capitán, unos Leviatanes"
     elif creature == "Hidras":
         message = "¡Ahoy! capitán, Unas Hidras"
-    else:
-        print("")
-        
     
+    # concatenate the second part of the final message based on the choossed direction
+    if direction == "babor":
+        message = message + " a babor"
+    elif direction == "estribor":
+        message = message + " a estribor"
+    elif direction == "proa":
+        message = message + " por la proa"
+    elif direction == "popa":
+        message = message + " por la popa"
+
+    # Show the final message
+    print(message) 
+    print() 
+
+    # Condition for stopping the program
+    if input().lower() == "stop":
+        stop = True
